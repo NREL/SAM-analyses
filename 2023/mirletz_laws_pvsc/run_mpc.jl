13f3dbee180c8905fb7f1db3274811ef4041f2ee
Nick Laws <laws.nick@gmail.com>
2023-05-12 08:59:58 -0600

    adapt run_mpc.jl for any computer
    but Apple silicon not supported for the SAM C libraries in REoptLite

--- 2023/mirletz_laws_pvsc/run_mpc.jl
+++ 2023/mirletz_laws_pvsc/run_mpc.jl
@@ -1,11 +1,19 @@
-reopt_repo = raw"C:\Users\bmirletz\source\repos\REoptLite"
-local_path = raw"C:\Users\bmirletz\source\repos\sam_dev\sam-analyses\2023\mirletz_laws_pvsc"
+#=
+This code uses a branch of REopt.jl, which is deprecated.
+To get the source code:
+```sh
+git clone https://github.com/NREL/REopt.git
+git checkout REopt.run_sam_battery_stateful
+```
+Then add the REoptLite code to your Julia env
+```julia
+pkg> add your/path/to/REoptLite/  # that you just cloned
+```
 
+NOTE the SAM libraries in REoptLite for Mac do not support Apple chips.
+=#
+using REopt, JuMP, HiGHS, JSON, DelimitedFiles, CSV, DataFrames
 
-include(string(reopt_repo, raw"\src\REopt.jl"))
-include(string(reopt_repo, raw"\src\mpc\sam_battery.jl"))
-
-using Main.REopt, JuMP, HiGHS, JSON, DelimitedFiles, CSV, DataFrames
 
 # Only need to account for battery errors at this stage, PV and load errors should be accounted for in previous function
 function update_electric_tariff_demand(results::Dict, inputs::Dict, start_index::Integer, next_start::Integer, batt_forecast_error::Vector{Float64})
@@ -147,135 +155,152 @@ function adjust_ac_power_for_forecast(results::Dict{String, Any}, ac_power::Vect
     return batt_power_series
 end
 
-input_data = JSON.parsefile(string(local_path, raw"\reopt_results\reopt_results_outage_True_True_18.389_-66.0933_match_sam.json"))
 
-site_dict = input_data["inputs"]["Scenario"]["Site"]
+function main(last_time_step = 8760)
+    input_data = JSON.parsefile(joinpath("reopt_results", "reopt_results_outage_True_True_18.389_-66.0933_match_sam.json"))
 
-pv_dict = site_dict["PV"]
-pv_capacity = pv_dict["max_kw"] # Needs to change to process sized outputs
-prod_factors = pv_dict["prod_factor_series_kw"]
+    site_dict = input_data["inputs"]["Scenario"]["Site"]
 
-batt_dict = site_dict["Storage"]
-batt_capacity = batt_dict["max_kwh"] # Needs to change to process sized outputs
-batt_power_kw = batt_dict["max_kw"]
-grid_charge = batt_dict["canGridCharge"]
-inv_eff = batt_dict["inverter_efficiency_pct"]
-rec_eff = batt_dict["rectifier_efficiency_pct"]
-internal_eff = batt_dict["internal_efficiency_pct"]
-soc = batt_dict["soc_init_pct"]
+    pv_dict = site_dict["PV"]
+    pv_capacity = pv_dict["max_kw"] # Needs to change to process sized outputs
+    prod_factors = pv_dict["prod_factor_series_kw"]
 
-charge_eff = rec_eff * sqrt(internal_eff)
-discharge_eff = inv_eff * sqrt(internal_eff)
+    batt_dict = site_dict["Storage"]
+    batt_capacity = batt_dict["max_kwh"] # Needs to change to process sized outputs
+    batt_power_kw = batt_dict["max_kw"]
+    grid_charge = batt_dict["canGridCharge"]
+    inv_eff = batt_dict["inverter_efficiency_pct"]
+    rec_eff = batt_dict["rectifier_efficiency_pct"]
+    internal_eff = batt_dict["internal_efficiency_pct"]
+    soc = batt_dict["soc_init_pct"]
 
-loads = site_dict["LoadProfile"]["loads_kw"]
+    charge_eff = rec_eff * sqrt(internal_eff)
+    discharge_eff = inv_eff * sqrt(internal_eff)
 
-generator = site_dict["Generator"]
-run_during_outage = generator["generator_only_runs_during_grid_outage"]
-gen_kw = generator["max_kw"]
+    loads = site_dict["LoadProfile"]["loads_kw"]
 
-tariff_label = site_dict["ElectricTariff"]["urdb_label"]
-tariff = REopt.URDBrate("5bfdc7925457a33744146c53", 2018)
+    generator = site_dict["Generator"]
+    run_during_outage = generator["generator_only_runs_during_grid_outage"]
+    gen_kw = generator["max_kw"]
 
-periods_per_month = floor(Int, size(tariff.tou_demand_rates)[1] / 12)
+    tariff_label = site_dict["ElectricTariff"]["urdb_label"]
+    tariff = REopt.URDBrate("5bfdc7925457a33744146c53", 2018)
 
-# Hours
-global start_index = 1
-horizon = 24
-interval = 1
+    periods_per_month = floor(Int, size(tariff.tou_demand_rates)[1] / 12)
 
-scenario_dict = Dict("PV" => Dict("size_kw" => pv_capacity, "production_factor_series" => zeros(horizon)),
-                    "ElectricStorage" => Dict("size_kw" => batt_power_kw, "size_kwh" => batt_capacity, "can_grid_charge" => grid_charge, "soc_init_fraction" => soc, "charge_efficiency" => charge_eff, "discharge_efficiency" => discharge_eff),
-                    "ElectricLoad" => Dict("loads_kw" => zeros(horizon)),
-                    "ElectricUtility" => Dict(),
-                    "ElectricTariff" => Dict("net_metering" => false, "export_rates" => zeros(horizon), "monthly_previous_peak_demands" => [0.0], "tou_previous_peak_demands" => zeros(periods_per_month)))
+    # Hours
+    start_index = 1
+    horizon = 24
+    interval = 1
 
-# TODO - get SAM battery SOC to match scenario_dict above
-# SAM_Battery class will resize appropriately given the scenarios above
-batt = SAM_Battery(string(local_path, raw".\test_batt.json"), batt_capacity)
+    scenario_dict = Dict(
+        "PV" => Dict("size_kw" => pv_capacity, "production_factor_series" => zeros(horizon)),
+        "ElectricStorage" => Dict(
+            "size_kw" => batt_power_kw, 
+            "size_kwh" => batt_capacity, 
+            "can_grid_charge" => grid_charge, 
+            "soc_init_fraction" => soc, 
+            "charge_efficiency" => charge_eff, 
+            "discharge_efficiency" => discharge_eff
+        ),
+        "ElectricLoad" => Dict("loads_kw" => zeros(horizon)),
+        "ElectricUtility" => Dict(),
+        "ElectricTariff" => Dict(
+            "net_metering" => false, 
+            "export_rates" => zeros(horizon), 
+            "monthly_previous_peak_demands" => [0.0], 
+            "tou_previous_peak_demands" => zeros(periods_per_month)
+        )
+    )
 
-actual_pv_df = CSV.read(string(local_path, raw".\pv_production_actual.csv"), DataFrame) # 25 years of data
-forecast_pv_df = CSV.read(string(local_path, raw".\pv_production_forecast.csv"), DataFrame) # 25 years of data
+    # TODO - get SAM battery SOC to match scenario_dict above
+    # REopt.SAM_Battery class will resize appropriately given the scenarios above
+    batt = REopt.SAM_Battery("test_batt.json", batt_capacity)
 
-actual_load_df = CSV.read(string(local_path, raw".\weather_and_load\san_juan_hospital_actual_load.csv"), DataFrame)
-forecast_load_df = CSV.read(string(local_path, raw".\weather_and_load\san_juan_hospital_forecast_load.csv"), DataFrame)
+    actual_pv_df = CSV.read("pv_production_actual.csv", DataFrame) # 25 years of data
+    forecast_pv_df = CSV.read("pv_production_forecast.csv", DataFrame) # 25 years of data
 
-outage_start = 4543
-outage_end = 4567
+    actual_load_df = CSV.read(joinpath("weather_and_load", "san_juan_hospital_actual_load.csv"), DataFrame)
+    forecast_load_df = CSV.read(joinpath("weather_and_load", "san_juan_hospital_forecast_load.csv"), DataFrame)
 
-forecast_output_powers = zeros(0)
-actual_output_powers = zeros(0)
+    outage_start = 4543
+    outage_end = 4567
 
-while start_index < 8761
-    end_index = min(8760, start_index + horizon - 1)
-    rate_dict = REopt.get_subset_of_urdb(tariff, start_index, end_index)
-    for (key, value) in rate_dict
-        scenario_dict["ElectricTariff"][key] = value
+    forecast_output_powers = zeros(0)
+    actual_output_powers = zeros(0)
+
+    while start_index < last_time_step + 1
+        end_index = min(last_time_step, start_index + horizon - 1)
+        rate_dict = REopt.get_subset_of_urdb(tariff, start_index, end_index)
+        for (key, value) in rate_dict
+            scenario_dict["ElectricTariff"][key] = value
+        end
+        # Forecast
+    
+        soc_init = scenario_dict["ElectricStorage"]["soc_init_fraction"]
+        scenario_dict["ElectricStorage"]["soc_min_fraction"] = min(0.7, soc_init)
+        scenario_dict["ElectricTariff"]["export_rates"] = zeros(end_index - start_index + 1)
+
+        """ SAM will deal with grid outage, none of the other algorithms get advance notice
+        if start_index - outage_start > 0 && start_index <= outage_end
+            scenario_dict["ElectricUtility"] = Dict()
+            outage_ts = max(1, outage_start - start_index)
+            outage_end_ts = max(1, outage_end - start_index)
+            scenario_dict["ElectricUtility"]["outage_start_time_step"] = outage_ts
+            scenario_dict["ElectricUtility"]["outage_end_time_step"] = outage_end_ts
+        else
+            delete!(scenario_dict, "ElectricUtility")
+        end
+        """
+
+        forecast_pv = forecast_pv_df[start_index:end_index, :Power]
+        forecast_load = forecast_load_df[start_index:end_index, :Load]
+
+        scenario_dict["PV"]["production_factor_series"] = clamp!(forecast_pv / scenario_dict["PV"]["size_kw"], 0.0, 10000)
+        scenario_dict["ElectricLoad"]["loads_kw"] = forecast_load
+
+        actual_pv = actual_pv_df[start_index:end_index, :Power]
+        actual_load = actual_load_df[start_index:end_index, :Load]
+
+        model = Model(HiGHS.Optimizer)
+        results = REopt.run_mpc(model, scenario_dict)
+        ac_batt_power = get_ac_batt_power(results)
+
+        pv_forecast_error = forecast_pv - actual_pv
+        load_forecast_error = forecast_load - actual_load
+        
+        adjust_ac_power_for_forecast(results, ac_batt_power, pv_forecast_error, load_forecast_error)
+
+        batt_power = REopt.get_batt_power_time_series(results, inv_eff, rec_eff)
+
+        actual_power = REopt.run_sam_battery(batt, batt_power)
+        print(actual_power)
+        ac_actual_power = REopt.dc_to_ac_power(actual_power, inv_eff, rec_eff)
+        REopt.update_mpc_from_batt_stateful(batt, scenario_dict)
+
+        # Compare ac_actual power to ac
+        # Positive - charged less than expected; negative - discharged less than expected
+        batt_forecast_error = ac_batt_power - ac_actual_power
+        
+        update_electric_tariff_demand(results, scenario_dict, start_index, start_index + interval, batt_forecast_error)
+        
+        # Want to save both of these timeseries for analysis
+        if (interval < horizon)
+            append!(forecast_output_powers, ac_batt_power[1:interval])
+            append!(actual_output_powers, actual_power[1:interval])
+        else
+            append!(forecast_output_powers, ac_batt_power)
+            append!(actual_output_powers, actual_power)
+        end
+        
+        print("Start time ", start_index, "\n")
+        print("MPC results:\n", results)
+        print("\n\n")
+        print("Next inputs: \n", scenario_dict)
+        print("\n\n")
+        start_index += interval
     end
-    # Forecast
- 
-    soc_init = scenario_dict["ElectricStorage"]["soc_init_fraction"]
-    scenario_dict["ElectricStorage"]["soc_min_fraction"] = min(0.7, soc_init)
-    scenario_dict["ElectricTariff"]["export_rates"] = zeros(end_index - start_index + 1)
 
-    """ SAM will deal with grid outage, none of the other algorithms get advance notice
-    if start_index - outage_start > 0 && start_index <= outage_end
-        scenario_dict["ElectricUtility"] = Dict()
-        outage_ts = max(1, outage_start - start_index)
-        outage_end_ts = max(1, outage_end - start_index)
-        scenario_dict["ElectricUtility"]["outage_start_time_step"] = outage_ts
-        scenario_dict["ElectricUtility"]["outage_end_time_step"] = outage_end_ts
-    else
-        delete!(scenario_dict, "ElectricUtility")
-    end
-    """
-
-    forecast_pv = forecast_pv_df[start_index:end_index, :Power]
-    forecast_load = forecast_load_df[start_index:end_index, :Load]
-
-    scenario_dict["PV"]["production_factor_series"] = clamp!(forecast_pv / scenario_dict["PV"]["size_kw"], 0.0, 10000)
-    scenario_dict["ElectricLoad"]["loads_kw"] = forecast_load
-
-    actual_pv = actual_pv_df[start_index:end_index, :Power]
-    actual_load = actual_load_df[start_index:end_index, :Load]
-
-    local model = Model(HiGHS.Optimizer)
-    results = REopt.run_mpc(model, scenario_dict)
-    local ac_batt_power = get_ac_batt_power(results)
-
-    pv_forecast_error = forecast_pv - actual_pv
-    load_forecast_error = forecast_load - actual_load
-    
-    adjust_ac_power_for_forecast(results, ac_batt_power, pv_forecast_error, load_forecast_error)
-
-    local batt_power = get_batt_power_time_series(results, inv_eff, rec_eff)
-
-    local actual_power = run_sam_battery(batt, batt_power)
-    print(actual_power)
-    local ac_actual_power = dc_to_ac_power(actual_power, inv_eff, rec_eff)
-    update_mpc_from_batt_stateful(batt, scenario_dict)
-
-    # Compare ac_actual power to ac
-    # Positive - charged less than expected; negative - discharged less than expected
-    local batt_forecast_error = ac_batt_power - ac_actual_power
-    
-    update_electric_tariff_demand(results, scenario_dict, start_index, start_index + interval, batt_forecast_error)
-    
-    # Want to save both of these timeseries for analysis
-    if (interval < horizon)
-        append!(forecast_output_powers, ac_batt_power[1:interval])
-        append!(actual_output_powers, actual_power[1:interval])
-    else
-        append!(forecast_output_powers, ac_batt_power)
-        append!(actual_output_powers, actual_power)
-    end
-    
-    print("Start time ", start_index, "\n")
-    print("MPC results:\n", results)
-    print("\n\n")
-    print("Next inputs: \n", scenario_dict)
-    print("\n\n")
-    global start_index += interval
+    DelimitedFiles.writedlm("output_powers_sj_hospital_day_ahead_forecast_ac.csv", forecast_output_powers, ',')
+    DelimitedFiles.writedlm("output_powers_sj_hospital_day_ahead_actual_dc.csv", actual_output_powers, ',')
 end
-
-DelimitedFiles.writedlm(string(local_path, raw".\output_powers_sj_hospital_day_ahead_forecast_ac.csv"), forecast_output_powers, ',')
-DelimitedFiles.writedlm(string(local_path, raw".\output_powers_sj_hospital_day_ahead_actual_dc.csv"), actual_output_powers, ',')
